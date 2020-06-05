a0403da67477698e37cb13eabc4c3f7e08f15905
Alexandre <alexsantsjrr@gmail.com>
2020-06-04 21:15:37 -0300

    Item collection option added

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,12 +24,15 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value> Selecione a Cidade </option>"
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => {return res.json()})
     .then( cities => {
         
         for(city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -39,3 +42,35 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+// Itens de coleta
+
+const itensToCollect = document.querySelectorAll(".itens-grid li")
+
+for (item of itensToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+let selectItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+    //verificar se existem items selecionados, se sim
+    //pegar os itens selecionados
+
+    const alreadySelected = selectItems.findIndex(function(item){
+        
+    })
+
+    //se já estiver selecionado, tirar da seleção
+
+    //se não estiver selecionado, adicionar a seleção
+
+    //atualizar o campo escondido com os itens selecionados
+}
